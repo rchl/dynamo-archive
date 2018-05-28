f1d1bf3d8435d24a2a94455b8747a218faeed4d3
Rafał Chłodnicki <rafal.chlodnicki@tidal.com>
2018-05-28 14:32:16 +0200

    Fix typo in command line help

--- bin/dynamo-restore.js
+++ bin/dynamo-restore.js
@@ -22,7 +22,7 @@ var argv = utils.config({
     demand: ['table'],
     optional: ['rate', 'key', 'secret', 'region'],
     usage: 'Restores Dynamo DB table from JSON file\n' +
-           'Usage: dynamo-archive --table my-table [--rate 100] [--region us-east-1] [--key AK...AA] [--secret 7a...IG]'
+           'Usage: dynamo-restore --table my-table [--rate 100] [--region us-east-1] [--key AK...AA] [--secret 7a...IG]'
 });
 
 var dynamo = utils.dynamo(argv);
